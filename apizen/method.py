7719d978355c614c2afdc120b1c8e5d23426ce77
Matrix <codecolor@outlook.com>
2017-05-21 15:56:55 +0800

    去除部分异常处理

--- apizen/method.py
+++ apizen/method.py
@@ -90,38 +90,38 @@ class Method:
     @staticmethod
     def run(version, method_name, request_method, request_params):
 
-        try:
-            # 最终传递给接口处理方法的全部参数
-            func_args = {}
-            # 获取接口处理方法
-            api_method = Method.get(version=version,
-                                    method_name=method_name,
-                                    request_method=request_method)
-            # 获取函数方法的参数
-            api_method_params = signature(api_method).parameters
+        # try:
+        # 最终传递给接口处理方法的全部参数
+        func_args = {}
+        # 获取接口处理方法
+        api_method = Method.get(version=version,
+                                method_name=method_name,
+                                request_method=request_method)
+        # 获取函数方法的参数
+        api_method_params = signature(api_method).parameters
 
-            for k, v in api_method_params.items():
-                if str(v.kind) == 'VAR_POSITIONAL':
-                    raise ApiSysError.error_api_config
-                elif str(v.kind) in ('POSITIONAL_OR_KEYWORD', 'KEYWORD_ONLY'):
-                    if k not in request_params:
-                        if v.default is Parameter.empty:
-                            missing_arguments = ApiSysError.missing_arguments
-                            missing_arguments.message = '{0}：{1}'.format(missing_arguments.message, k)
-                            raise missing_arguments
-                        func_args[k] = Method.convert(k, v.default, v.default, v.annotation)
-                    else:
-                        func_args[k] = Method.convert(k, request_params.get(k), v.default, v.annotation)
-                elif str(v.kind) == 'VAR_KEYWORD':
-                    func_args.update({k: v for k, v in request_params.items()
-                                      if k not in api_method_params.keys()})
+        for k, v in api_method_params.items():
+            if str(v.kind) == 'VAR_POSITIONAL':
+                raise ApiSysError.error_api_config
+            elif str(v.kind) in ('POSITIONAL_OR_KEYWORD', 'KEYWORD_ONLY'):
+                if k not in request_params:
+                    if v.default is Parameter.empty:
+                        missing_arguments = ApiSysError.missing_arguments
+                        missing_arguments.message = '{0}：{1}'.format(missing_arguments.message, k)
+                        raise missing_arguments
+                    func_args[k] = Method.convert(k, v.default, v.default, v.annotation)
+                else:
+                    func_args[k] = Method.convert(k, request_params.get(k), v.default, v.annotation)
+            elif str(v.kind) == 'VAR_KEYWORD':
+                func_args.update({k: v for k, v in request_params.items()
+                                  if k not in api_method_params.keys()})
 
-            return api_method(**func_args)
-        except ApiError as ex:
-            raise ex
-        except JSONDecodeError:
-            raise ApiSysError.invalid_json
-        except Exception as ex:
-            api_ex = ApiSysError.system_error
-            api_ex.message = '{0}：{1}'.format(api_ex.message, ex)
-            raise api_ex
+        return api_method(**func_args)
+        # except ApiError as ex:
+        #     raise ex
+        # except JSONDecodeError:
+        #     raise ApiSysError.invalid_json
+        # except Exception as ex:
+        #     api_ex = ApiSysError.system_error
+        #     api_ex.message = '{0}：{1}'.format(api_ex.message, ex)
+        #     raise api_ex
