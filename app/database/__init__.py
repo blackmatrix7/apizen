16f5d19a64e60d2df6eb7c2d239cccb5ae9e4e5c
Matrix <codecolor@outlook.com>
2017-05-29 02:10:01 +0800

    全新的apizen更以flask呈现

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -19,7 +19,26 @@ class ModelBase:
 
 
 class ModelMixin:
-    pass
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        return instance.save()
+
+    def update(self, commit=True, **kwargs):
+        for attr, value in kwargs.():
+            setattr(self, attr, value)
+        return commit and self.save() or self
+
+    def save(self, commit=True):
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
+
+    def delete(self, commit=True):
+        db.session.delete(self)
+        return commit and db.session.commit()
 
 
 if __name__ == '__main__':
