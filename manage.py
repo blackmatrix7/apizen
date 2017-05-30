a3fb53b7e29018df68dd20f3bb897e77670a8216
Matrix <codecolor@outlook.com>
2017-05-30 20:31:45 +0800

    增加flask-migrate

--- manage.py
+++ manage.py
@@ -6,25 +6,25 @@
 # @File    : manage.py
 # @Software: PyCharm
 import os
+import app.models
 from app import create_app
 from app.database import db
 from flask_script import Manager
-from app.models.oauth import OAuthClient
 from flask_migrate import Migrate, MigrateCommand
 
 __author__ = 'blackmatrix'
 
 
 cfg_name = os.environ.get('APIZEN_CFG', 'default')
-app = create_app(cfg_name)
-migrate = Migrate(app, db)
-manager = Manager(app)
+flask_app = create_app(cfg_name)
+migrate = Migrate(flask_app, db)
+manager = Manager(flask_app)
 manager.add_command('db', MigrateCommand)
 
 
 @manager.command
 def devserver():
-    app.run()
+    flask_app.run()
 
 
 @manager.command
