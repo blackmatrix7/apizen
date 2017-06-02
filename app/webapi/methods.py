57b04d4fa8ef906ab29144ff2f77e3fd9c645d1f
Matrix <codecolor@outlook.com>
2017-06-02 13:41:23 +0800

    演示demo增加注释说明

--- app/webapi/methods.py
+++ app/webapi/methods.py
@@ -14,12 +14,19 @@ __author__ = 'blackmatrix'
 @version(1.0)
 class ApiMethodsV10(ApiMethodsBase):
     api_methods = {
+        # 第一个demo
         'matrix.api.set-user': {'func': demo.set_user},
+        # 以list的方式传值
         'matrix.api.set-users': {'func': demo.set_users},
+        # 操作数据库
         'matrix.api.set-client': {'func': demo.set_client},
+        # 抛出一个异常
         'matrix.api.return-err': {'func': demo.raise_error},
+        # 错误的函数编写
         'matrix.api.err-func': {'func': demo.err_func},
+        # 实例方法调用
         'matrix.api.instance-func': {'func': demo.instance_func},
+        # 类方法调用
         'matrix.api.class-func': {'func': demo.class_method},
     }
 
@@ -27,17 +34,22 @@ class ApiMethodsV10(ApiMethodsBase):
 @version(1.1)
 class ApiMethodsV11(ApiMethodsV10):
     api_methods = {
+        # 传递任意参数
         'matrix.api.send-kwargs': {'func': demo.send_kwargs},
-        'matrix.api.raise-error': {'func': demo.raise_error},
-        'matrix.api.speed-test': {'func': demo.speed_test},
+        # API版本继承
+        'matrix.api.raise-error': {'func': demo.raise_error}
     }
 
 
 @version(1.2)
 class ApiMethodsV12(ApiMethodsV11):
     api_methods = {
+        # 只允许POST请求
         'matrix.api.only-post': {'func': demo.raise_error, 'methods': ['post']},
-        'matrix.api.api-stop': {'func': demo.raise_error, 'enable': False}
+        # 停用API
+        'matrix.api.api-stop': {'func': demo.raise_error, 'enable': False},
+        # 自定义一个异常信息
+        'matrix.api.custom-error': {'func': demo.custom_error}
     }
 
 
--- app/webapi/services/demo.py
+++ app/webapi/services/demo.py
@@ -95,6 +95,15 @@ class ApiDemo:
         raise ApiSubExceptions.unknown_error
 
     @staticmethod
+    def custom_error():
+        """
+        接口抛出异常的使用说明，抛出异常信息后，会在返回接口的code中显示对应异常的编号，
+        同时，返回的http code 也会根据异常配置中的status_code而改变
+        :return:  返回异常信息
+        """
+        raise ApiSubExceptions.unknown_error('这是一个自定义异常信息')
+
+    @staticmethod
     def send_kwargs(value: str, **kwargs):
         """
         VAR_KEYWORD 参数类型的传值测试，传入任意k/wc，会在调用结果中返回
