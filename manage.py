da53708c37adb96f8e35c6c523b9a284736f4725
Matrix <codecolor@outlook.com>
2017-06-14 22:57:24 +0800

    gunicorn测试

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@
 # @Software: PyCharm
 from app import create_app
 from app.database import db
-from flask_script import Manager, Command, Option
+from flask_script import Manager
 from app.database.models import *
 from app.user.controller import new_user
 from flask_migrate import MigrateCommand
@@ -15,30 +15,9 @@ from flask_migrate import MigrateCommand
 __author__ = 'blackmatrix'
 
 
-class GunicornServer(Command):
-    """Run the app within Gunicorn"""
-
-    def get_options(self):
-        from gunicorn.config import make_settings
-
-        settings = make_settings()
-        options = (
-            Option(*klass.cli, action=klass.action)
-            for setting, klass in settings.iteritems() if klass.cli
-        )
-        return options
-
-    def run(self, *args, **kwargs):
-        from gunicorn.app.wsgiapp import WSGIApplication
-
-        app = WSGIApplication()
-        app.app_uri = 'manage:app'
-        return app.run()
-
 manager = Manager(create_app)
 manager.add_command('db', MigrateCommand)
 manager.add_option('-c', '--config', dest='app_config', required=False)
-manager.add_command("gunicorn", GunicornServer())
 
 
 @manager.command
