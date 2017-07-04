3a8221314220acfcf68132b0f39b71b2a6267f90
Matrix <codecolor@outlook.com>
2017-07-04 19:57:18 +0800

    多线程写入日志

--- manage.py
+++ manage.py
@@ -32,7 +32,10 @@ if not os.path.exists('logs'):
 # 默认级别为ERROR，设置为DEBUG，记录INFO和DEBUG级别的日志
 logging.basicConfig(level=logging.DEBUG)
 # TODO 解决多进程日志问题
-fh = TimedRotatingFileHandler('logs/manage.log', 'midnight', 1, 10)
+from cloghandler import ConcurrentRotatingFileHandler
+logfile = os.path.abspath('logs/manage.log')
+fh = ConcurrentRotatingFileHandler(logfile, "a", 512*1024, 5)
+# fh = TimedRotatingFileHandler('logs/manage.log', 'midnight', 1, 10)
 ch = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s - [%(filename)s:%(lineno)s]')
 fh.setFormatter(formatter)
