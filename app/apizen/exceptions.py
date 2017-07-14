8198ac0a89da89db3cba41c481d447b3c9092f0c
Matrix <codecolor@outlook.com>
2017-07-14 14:13:34 +0800

    简化异常的处理方式，提高代码可读性

--- app/apizen/exceptions.py
+++ app/apizen/exceptions.py
@@ -42,9 +42,9 @@ class ZenException(Exception):
 
 class ApiException:
 
-    def __init__(self, err_code, http_code, err_msg, ex_type=Exception):
+    def __init__(self, err_code, err_msg, http_code, err_type=Exception):
         self.err_msg = err_msg
-        self.ex_type = ex_type
+        self.ex_type = err_type
         self.err_code = err_code
         self.http_code = http_code
 
@@ -59,59 +59,59 @@ class ApiException:
         return api_ex
     
 
-# API 系统层面异常信息，以1000开始
+# API 系统层面异常信息
 class ApiSysExceptions:
-    # code 1000 为保留编码，代表执行成功
+    # code 1000 为保留编码，代表执行成功，异常信息以1001开始
     # 服务不可用
-    missing_system_error = ApiException(err_code=1001, http_code=403, err_msg='服务不可用', ex_type=Exception)
+    missing_system_error = ApiException(err_code=1001, http_code=403, err_msg='服务不可用', err_type=Exception)
     # 限制时间内调用失败次数
-    app_call_limited = ApiException(err_code=1002, http_code=403, err_msg='限制时间内调用失败次数', ex_type=Exception)
+    app_call_limited = ApiException(err_code=1002, http_code=403, err_msg='限制时间内调用失败次数', err_type=Exception)
     # 请求被禁止
-    forbidden_request = ApiException(err_code=1003, http_code=403, err_msg='请求被禁止', ex_type=Exception)
+    forbidden_request = ApiException(err_code=1003, http_code=403, err_msg='请求被禁止', err_type=Exception)
     # 缺少版本参数
-    missing_version = ApiException(err_code=1004, http_code=400, err_msg='缺少版本参数', ex_type=KeyError)
+    missing_version = ApiException(err_code=1004, http_code=400, err_msg='缺少版本参数', err_type=KeyError)
     # 不支持的版本号
-    unsupported_version = ApiException(err_code=1005, http_code=400, err_msg='不支持的版本号', ex_type=ValueError)
+    unsupported_version = ApiException(err_code=1005, http_code=400, err_msg='不支持的版本号', err_type=ValueError)
     # 非法的版本参数
-    version_stop = ApiException(err_code=1006, http_code=400, err_msg='接口版本已停用', ex_type=ValueError)
+    version_stop = ApiException(err_code=1006, http_code=400, err_msg='接口版本已停用', err_type=ValueError)
     # 缺少时间戳参数
-    missing_timestamp = ApiException(err_code=1007, http_code=400, err_msg='缺少时间戳参数', ex_type=KeyError)
+    missing_timestamp = ApiException(err_code=1007, http_code=400, err_msg='缺少时间戳参数', err_type=KeyError)
     # 非法的时间戳参数
-    invalid_timestamp = ApiException(err_code=1008, http_code=400, err_msg='非法的时间戳参数', ex_type=ValueError)
+    invalid_timestamp = ApiException(err_code=1008, http_code=400, err_msg='非法的时间戳参数', err_type=ValueError)
     # 缺少签名参数
-    missing_signature = ApiException(err_code=1009, http_code=400, err_msg='缺少签名参数', ex_type=KeyError)
+    missing_signature = ApiException(err_code=1009, http_code=400, err_msg='缺少签名参数', err_type=KeyError)
     # 无效签名
-    invalid_signature = ApiException(err_code=1010, http_code=400, err_msg='无效签名', ex_type=ValueError)
+    invalid_signature = ApiException(err_code=1010, http_code=400, err_msg='无效签名', err_type=ValueError)
     # 无效数据格式
-    invalid_format = ApiException(err_code=1011, http_code=400, err_msg='无效数据格式', ex_type=ValueError)
+    invalid_format = ApiException(err_code=1011, http_code=400, err_msg='无效数据格式', err_type=ValueError)
     # 缺少方法名参数
-    missing_method = ApiException(err_code=1012, http_code=400, err_msg='缺少方法名参数', ex_type=KeyError)
+    missing_method = ApiException(err_code=1012, http_code=400, err_msg='缺少方法名参数', err_type=KeyError)
     # 不存在的方法名
-    invalid_method = ApiException(err_code=1013, http_code=404, err_msg='不存在的方法名', ex_type=AttributeError)
+    invalid_method = ApiException(err_code=1013, http_code=404, err_msg='不存在的方法名', err_type=AttributeError)
     # 缺少access_token参数
-    missing_access_token = ApiException(err_code=1014, http_code=400, err_msg='缺少access_token参数', ex_type=KeyError)
+    missing_access_token = ApiException(err_code=1014, http_code=400, err_msg='缺少access_token参数', err_type=KeyError)
     # 无效access_token
-    invalid_access_token = ApiException(err_code=1015, http_code=401, err_msg='无效access_token', ex_type=ValueError)
+    invalid_access_token = ApiException(err_code=1015, http_code=401, err_msg='无效access_token', err_type=ValueError)
     # api已经停用
-    api_stop = ApiException(err_code=1016, http_code=405, err_msg='api已经停用', ex_type=Exception)
+    api_stop = ApiException(err_code=1016, http_code=405, err_msg='api已经停用', err_type=Exception)
     # 系统处理错误
-    system_error = ApiException(err_code=1017, http_code=500, err_msg='系统处理错误', ex_type=Exception)
+    system_error = ApiException(err_code=1017, http_code=500, err_msg='系统处理错误', err_type=Exception)
     # 缺少方法所需参数
-    missing_arguments = ApiException(err_code=1018, http_code=400, err_msg='缺少方法所需参数', ex_type=KeyError)
+    missing_arguments = ApiException(err_code=1018, http_code=400, err_msg='缺少方法所需参数', err_type=KeyError)
     # 不支持的http请求方式
-    not_allowed_request = ApiException(err_code=1019, http_code=405, err_msg='不支持的http请求方式', ex_type=Exception)
+    not_allowed_request = ApiException(err_code=1019, http_code=405, err_msg='不支持的http请求方式', err_type=Exception)
     # 错误的API配置
-    error_api_config = ApiException(err_code=1020, http_code=500, err_msg='错误的API配置', ex_type=NotImplementedError)
+    error_api_config = ApiException(err_code=1020, http_code=500, err_msg='错误的API配置', err_type=NotImplementedError)
     # 无效的json格式
-    invalid_json = ApiException(err_code=1021, http_code=400, err_msg='错误或不合法的json格式', ex_type=JSONDecodeError)
+    invalid_json = ApiException(err_code=1021, http_code=400, err_msg='错误或不合法的json格式', err_type=JSONDecodeError)
     # 参数类型错误
-    error_args_type = ApiException(err_code=1022, http_code=400, err_msg='参数类型错误', ex_type=KeyError)
+    error_args_type = ApiException(err_code=1022, http_code=400, err_msg='参数类型错误', err_type=KeyError)
     # 缺少Content-Type
-    missing_content_type = ApiException(err_code=1023, http_code=400, err_msg='缺少Content-Type', ex_type=Exception)
+    missing_content_type = ApiException(err_code=1023, http_code=400, err_msg='缺少Content-Type', err_type=Exception)
     # 不被接受的Content-Type
-    unacceptable_content_type = ApiException(err_code=1024, http_code=400, err_msg='不被接受的Content-Type', ex_type=Exception)
+    unacceptable_content_type = ApiException(err_code=1024, http_code=400, err_msg='不被接受的Content-Type', err_type=Exception)
     # 无效的请求
-    bad_request = ApiException(err_code=1025, http_code=400, err_msg='无效的请求', ex_type=Exception)
+    bad_request = ApiException(err_code=1025, http_code=400, err_msg='无效的请求', err_type=Exception)
 
 if __name__ == '__main__':
     pass
