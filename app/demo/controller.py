d5e8f3fd5b9a975c36230e7db573549ed6f5f622
Matrix <codecolor@outlook.com>
2017-07-14 15:33:44 +0800

    修改自定义异常信息的demo

--- app/demo/controller.py
+++ app/demo/controller.py
@@ -112,13 +112,13 @@ class ApiDemo:
 
     # 演示自定义异常信息
     @staticmethod
-    def custom_error():
+    def custom_error(msg):
         """
         接口抛出异常的使用说明，抛出异常信息后，会在返回接口的code中显示对应异常的编号，
         同时，返回的http code 也会根据异常配置中的status_code而改变
         :return:  返回异常信息
         """
-        raise ApiSubExceptions.unknown_error('这是一个自定义异常信息')
+        raise ApiSubExceptions.unknown_error(msg)
 
     # 演示接口接收任意k/w参数
     @staticmethod
